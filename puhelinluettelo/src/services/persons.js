f83d4db6d7a26dcce25754357e1e937c4cc37de3
Simo Sirén <45048404+Point-SimoSiren@users.noreply.github.com>
2019-09-23 08:15:52 +0300

    axios liikenne ulkoistettu omaan kansioon

---
+++ puhelinluettelo/src/services/persons.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = async () => {
+    const req = axios.get(baseUrl)
+    return req.then(res => res.data)
+}
+
+const create = newContact => {
+    return axios.post(baseUrl, newContact)
+}
+
+const update = async (id, changedContact) => {
+    const req = axios.put(`${baseUrl}/${id}`, changedContact)
+    const res = await req
+    return res.data
+}
+
+const remove = id => axios.delete(`${baseUrl}/${id}`)
+
+export default {
+    getAll,
+    create,
+    update,
+    remove
+}
