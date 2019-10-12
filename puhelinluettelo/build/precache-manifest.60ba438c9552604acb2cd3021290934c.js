2d191254e5a8a6ee73cf265eef40be9daba5b227
Simo Sirén <45048404+Point-SimoSiren@users.noreply.github.com>
2019-10-12 09:10:06 +0300

    build ennen lintiä. kaikki toimii

---
+++ puhelinluettelo/build/precache-manifest.60ba438c9552604acb2cd3021290934c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6ea3e64eee413ea43f101aaa4c2892e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b213dd92e31c56add04",
+    "url": "/static/css/main.c46b9ff9.chunk.css"
+  },
+  {
+    "revision": "ed1f072e99b75cc8adbe",
+    "url": "/static/js/2.f4685d81.chunk.js"
+  },
+  {
+    "revision": "7b213dd92e31c56add04",
+    "url": "/static/js/main.8e934d93.chunk.js"
+  },
+  {
+    "revision": "235accea7dab4cb774d7",
+    "url": "/static/js/runtime~main.88d66cb1.js"
+  }
+]);
